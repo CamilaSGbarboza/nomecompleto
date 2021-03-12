9bc8220ddab94dee9df948eeb730fa78842d75e3
camila <camilasangobar@gmail.com>
2021-03-12 15:33:26 -0300

    nomecompleto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace nomecompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("olá");
+           Console.WriteLine("digite seu nome:");
+           string nome = Console.ReadLine();
+           Console.WriteLine();
+           Console.WriteLine("seu sobrenome");
+           string sobrenome = Console.ReadLine();
+           Console.WriteLine();
+           Console.Write("bem-vindo, ");
+           Console.Write(nome );
+           Console.Write(" ");
+           Console.Write(sobrenome);
+           Console.WriteLine();
+           Console.WriteLine($"{sobrenome.ToUpper()}, {nome.ToLower()}");
+
+        }
+    }
+}
